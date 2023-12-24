8dcbd41ec52e610ab2a45c7fd53cbcb5382d9e5b
ICOM <105383896+icomgroup@users.noreply.github.com>
2023-12-24 22:08:48 +0400

    Add files

---
+++ src/store/models/articles.js
@@ -0,0 +1,31 @@
+export default {
+  namespaced: true,
+
+  state: {
+    articles: [],
+  },
+
+  actions: {
+    fetchAll({ commit }) {
+      return window.axios.get('/articles')
+        .then((response) => {
+          commit('articles', response.data);
+          return response.data;
+        })
+    },
+
+    fetchById({ commit }, { id }) {
+      console.log(commit);
+      return window.axios.get(`/articles/${id}`)
+        .then((response) => {
+          return response.data;
+        })
+    }
+  },
+
+  mutations: {
+    articles(state, articles) {
+      state.articles = articles;
+    },
+  },
+}
